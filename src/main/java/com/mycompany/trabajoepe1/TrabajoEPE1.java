838a846984b8068aee575a41d2246204d4d07f29
Carlos Ancapi <carlos.ancapi@gmail.com>
2023-04-02 05:20:30 -0400

    primer diseño de interfaz

---
+++ src/main/java/com/mycompany/trabajoepe1/TrabajoEPE1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabajoepe1;
+
+/**
+ *
+ * @author ancap
+ */
+public class TrabajoEPE1 {
+
+    public static void main(String[] args) {
+        Ventana ventana1 = new Ventana();
+        ventana1.setVisible(true);
+    }
+}
